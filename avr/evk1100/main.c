04be1f8e19ae93aa146721db6d55a89d040bc714
Håkon Wikene <haakonwikene@gmail.com>
2012-09-19 14:41:57 +0200

    first test project for avr32 test board, evk1100

---
+++ avr/evk1100/main.c
@@ -0,0 +1,21 @@
+/********************************************************
+ Name          : main.c
+ Author        : Nomen Nescio
+ Copyright     : Not really
+ Description   : EVK1100 template
+ **********************************************************/
+
+// Include Files 
+#include "board.h"
+
+
+/* TODO: Add software framework include drivers below */
+//#include "gpio.h"
+//#include "adc.h"
+
+int main(void) {
+
+	/* TODO: replace this comment with your code */
+	
+	return 0;
+}
