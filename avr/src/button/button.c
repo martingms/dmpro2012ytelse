d40988b38616147cb6e739203a74b6a8430dd119
Erik Lothe <eriklot@stud.ntnu.no>
2012-10-03 14:45:24 +0200

    button

---
+++ avr/src/button/button.c
@@ -0,0 +1,111 @@
+#include "button.h"
+
+#include "gpio.h"
+#include "board.h"
+#include "intc.h"
+
+
+
+
+#define B0_VALUE gpio_get_pin_value(GPIO_PUSH_BUTTON_0)
+#define B1_VALUE gpio_get_pin_value(GPIO_PUSH_BUTTON_1)
+#define B2_VALUE gpio_get_pin_value(GPIO_PUSH_BUTTON_2)
+
+#define BUTTON_PUSHED 0
+#define BUTTON_RELEASED 1
+
+
+#define MAX_LISTENERS 20
+void(*listeners[MAX_LISTENERS]) (U8);
+int n_listeners;
+
+/*
+ * Notifies all listeners of a button push
+ */
+void notify_listeners(int buttons) {
+	int i;
+	for (i = 0; i < n_listeners; ++i) {
+		listeners[i](buttons);
+	}
+}
+
+void interrupt_routine(void) {
+	U8 buttons = 0;
+
+	if (B0_VALUE == BUTTON_PUSHED) buttons |= 1;
+	if (B1_VALUE == BUTTON_PUSHED) buttons |= 2;
+	if (B2_VALUE == BUTTON_PUSHED) buttons |= 4;
+	notify_listeners(buttons);
+
+	gpio_clear_pin_interrupt_flag(GPIO_PUSH_BUTTON_0);
+	gpio_clear_pin_interrupt_flag(GPIO_PUSH_BUTTON_1);
+	gpio_clear_pin_interrupt_flag(GPIO_PUSH_BUTTON_2);
+}
+
+/*
+ * Interrupt vector for button 0
+ */
+__attribute__((__interrupt__)) void i0(void) {
+	interrupt_routine();
+}
+
+/*
+ * Interrupt vector for button 1 and 2
+ */
+__attribute__((__interrupt__)) void i1(void) {
+	interrupt_routine();
+}
+
+/*
+ * Initializes the buttons with interrupts
+ */
+int button_init(void) {
+	int rc;
+
+	n_listeners = 0;
+
+	// Enable GPIO button pins
+	gpio_enable_gpio_pin(GPIO_PUSH_BUTTON_0);
+	gpio_enable_gpio_pin(GPIO_PUSH_BUTTON_1);
+	gpio_enable_gpio_pin(GPIO_PUSH_BUTTON_2);
+
+	// Enable glitch filter for all buttons
+	gpio_enable_pin_glitch_filter(GPIO_PUSH_BUTTON_0);
+	gpio_enable_pin_glitch_filter(GPIO_PUSH_BUTTON_1);
+	gpio_enable_pin_glitch_filter(GPIO_PUSH_BUTTON_2);
+
+	// Initializes the hardware interrupt controller driver
+	INTC_init_interrupts();
+
+	// Register interrupt for buttons
+	int b0_line = AVR32_GPIO_IRQ_0 + GPIO_PUSH_BUTTON_0 / 8;
+	int b1_b2_line = AVR32_GPIO_IRQ_0 + GPIO_PUSH_BUTTON_1 / 8;
+	// Because the lines for button 1 and button 2 are equal,
+	// the same interrupt vector are used.
+
+	INTC_register_interrupt(&i0, b0_line, AVR32_INTC_INT0);
+	INTC_register_interrupt(&i1, b1_b2_line, AVR32_INTC_INT0);
+
+
+	// GPIO enable interrupt of buttons on push
+	rc = gpio_enable_pin_interrupt(GPIO_PUSH_BUTTON_0, GPIO_FALLING_EDGE);
+	if (rc != GPIO_SUCCESS) return rc;
+	rc = gpio_enable_pin_interrupt(GPIO_PUSH_BUTTON_1, GPIO_FALLING_EDGE);
+	if (rc != GPIO_SUCCESS) return rc;
+	rc = gpio_enable_pin_interrupt(GPIO_PUSH_BUTTON_2, GPIO_FALLING_EDGE);
+	if (rc != GPIO_SUCCESS) return rc;
+
+
+	Enable_global_interrupt(); //TODO move this somewhere else
+	return 0;
+}
+
+int button_reg_listener(void(*listener) (U8)) {
+	if (++n_listeners >= MAX_LISTENERS){
+		return -1;
+	}
+
+	listeners[n_listeners-1] = listener;
+	return 0;
+}
+
