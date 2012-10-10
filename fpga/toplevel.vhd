f7dd79eedd4473b6fad0aa2a7bae1f871f44c67b
Vegard Edvardsen <evegard@hotmail.com>
2012-10-10 14:37:06 +0200

    Added initial FPGA ISE project

---
+++ fpga/toplevel.vhd
@@ -0,0 +1,42 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:28:36 10/10/2012 
+-- Design Name: 
+-- Module Name:    toplevel - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity toplevel is
+    Port ( clk : in  STD_LOGIC);
+end toplevel;
+
+architecture Behavioral of toplevel is
+
+begin
+
+
+end Behavioral;
+
