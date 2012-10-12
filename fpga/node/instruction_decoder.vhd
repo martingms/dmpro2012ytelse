4ed4101e637a02379d62a79aa682073adc07566c
unknown <hanskrfl@dmlab04.idipc.idi.ntnu.no>
2012-10-12 15:43:46 +0200

    FPGA simd node minor change

--- fpga/node/instruction_decoder.vhd
+++ fpga/node/instruction_decoder.vhd
@@ -25,24 +25,22 @@ use WORK.FPGA_CONSTANT_PKG.ALL;
 
 entity INSTRUCTION_DECODER is
 	Port (
-		clk 							: in  STD_LOGIC;
-		reset 						: in  STD_LOGIC;
+		op_code 						: in  STD_LOGIC_VECTOR (NODE_INST_OP-1 downto 0);
 		
-		op_code 						: in  STD_LOGIC_VECTOR (NODE_OP_CODE-1 downto 0);
-		
-		alu_ctrl 					: out  STD_LOGIC_VECTOR (1 downto 0);
-		alu_src 						: out  STD_LOGIC;
-		reg_src 						: out  STD_LOGIC;
-		com_out 						: out  STD_LOGIC;
-		reg_write					: out  STD_LOGIC;		
-		s_swap 						: out  STD_LOGIC;
+		-- Node control signals
+		alu_ctrl 					: out  STD_LOGIC_VECTOR (1 downto 0)						:= (others => '0'); 	-- controls alu operation
+		alu_src 						: out  STD_LOGIC													:= '0';					-- 0 = immidiate 	| 1 = reg data 1
+		reg_src 						: out  STD_LOGIC													:= '0';					-- 0 = alu res 	| 1 = n/s/e/w
+		com_out 						: out  STD_LOGIC													:= '0';					-- 0 = alu res		| 1 = n/s/e/w (algo)
+		reg_write					: out  STD_LOGIC													:= '0';					-- 0 = no write	| 1 = write
+		s_swap 						: out  STD_LOGIC													:= '0';					-- 0 = no swap		| 1 = swap
 		
 		-- Used for controlling the 4 way data exchange
-		reg_addr_src 				: out  STD_LOGIC := '0';
-		adr0 							: out  STD_LOGIC_VECTOR (NODE_REG_ADR-1 downto 0);
-		adr1 							: out  STD_LOGIC_VECTOR (NODE_REG_ADR-1 downto 0);
-		adr2 							: out  STD_LOGIC_VECTOR (NODE_REG_ADR-1 downto 0);
-		adr3 							: out  STD_LOGIC_VECTOR (NODE_REG_ADR-1 downto 0)
+		reg_addr_src 				: out  STD_LOGIC													:= '0';
+		adr0 							: out  STD_LOGIC_VECTOR (NODE_INST_REG-1 downto 0)		:= (others => '0');
+		adr1 							: out  STD_LOGIC_VECTOR (NODE_INST_REG-1 downto 0)		:= (others => '0');
+		adr2 							: out  STD_LOGIC_VECTOR (NODE_INST_REG-1 downto 0)		:= (others => '0');
+		adr3 							: out  STD_LOGIC_VECTOR (NODE_INST_REG-1 downto 0)		:= (others => '0')
 	);
 end INSTRUCTION_DECODER;
 
@@ -50,6 +48,20 @@ architecture Behavioral of INSTRUCTION_DECODER is
 
 begin
 
+	PROCESSOR_CONTROL: process(op_code) begin	
+		case op_code is
+			-- R-format instructions
+			when NODE_INSTR_OP_R =>
+				alu_ctrl				<= '0';
+				alu_src				<= '0';
+				reg_src				<= '0';
+				com_out				<= '0';
+				reg_write			<= '0';
+				s_swap				<= '0';
+			when others =>
+				-- do nothing
+		end case;
+	end process;
 
 end Behavioral;
 
