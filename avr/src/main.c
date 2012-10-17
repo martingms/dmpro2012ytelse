218ac11ff49deda3f89453e56a83b4ef1e56a3ab
Mads Sandvei <mads@madslaptop.(none)>
2012-10-17 11:12:55 +0200

    corrected a lot of seprintf calls in main

--- avr/src/main.c
+++ avr/src/main.c
@@ -84,7 +84,7 @@ int tmp(void)
 
 	LED_On(LED3);
 
-	seprintf(sprintf_buf, "The system has %d LUNs\n", get_nb_lun());
+	seprintf("The system has %d LUNs\n", get_nb_lun());
 
 	seprintf("trying to open file...\n");
 
@@ -99,54 +99,24 @@ int tmp(void)
 			char buf[512];
 
 			if ((read_bytes = read(fd, buf, fs)) == fs) {
-				seprintf(sprintf_buf, "the file was %d bytes big\n", fs);
+				seprintf("the file was %d bytes big\n", fs);
 
 				seprintf("This is the data we read:\n");
 
 				buf[fs] = '\0'; // lets assume fs != 512
 				seprintf(buf);
 			} else {
-				seprintf(sprintf_buf, "file is %d bytes, but I could only read %d!\n", fs, read_bytes);
+				seprintf("file is %d bytes, but I could only read %d!\n", fs, read_bytes);
 			}
 		} else {
-			seprintf(sprintf_buf, "file was bigger than 512b (%d), so not dumping over rs232\n", fs);
+			seprintf("file was bigger than 512b (%d), so not dumping over rs232\n", fs);
 		}
 		seprintf("========\ngoodbye\n========\n");
 		close(fd);
 	} else {
 		seprintf("failed to open file\n");
 	}
-	if(init_sram(FOSC0)){
-		const char bmp_test[] = "A:/1.bmp";
-		seprintf("Trying to open bmp_test\n");
-		fd = open(bmp_test,O_RDONLY);
-		if (fd >= 0) {
-			seprintf("Got it\n");
 
-			int fs = fsaccess_file_get_size(fd);
-			int read_bytes;
-			if (fs < 512000) {
-				seprintf("Trying to allocate memory:\n");
-				char *buf = (char *)malloc(fs);
-				assert_critical(buf>0);
-				seprintf(sprintf_buf, "allocated at %d\n", (int)buf);
-
-				if ((read_bytes = read(fd, buf, fs)) == fs) {
-					seprintf(sprintf_buf, "we read %d bytes\n", fs);
-				} else {
-					seprintf(sprintf_buf, "file is %d bytes, but I could only read %d!\n", fs, read_bytes);
-					read_bytes = read(fd, buf+read_bytes, fs);
-					seprintf(sprintf_buf, "Tried again and read %d more!\n", read_bytes);
-				}
-			}
-			else{
-				seprintf(sprintf_buf, "the file was %d bytes big\nThat's a little too big\n", fs);
-			}
-		}
-	}
-	else{
-		seprintf("Couldn't initialize sram properly.\n");
-	}
 	while (mmc_status() == CTRL_GOOD);
 	seprintf("SD disconnected!\n");
 
