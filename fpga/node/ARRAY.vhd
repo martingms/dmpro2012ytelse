28597d22f4daff051b16e62bd21279d50a805b6e
Hans Kristian <hans@starefossen.com>
2012-11-05 07:31:22 +0100

    continued working on the FPGA SIMD node array

--- fpga/node/ARRAY.vhd
+++ fpga/node/ARRAY.vhd
@@ -31,7 +31,10 @@ entity SIMD_ARRAY is
 		instr 						: in  STD_LOGIC_VECTOR (NODE_IDATA_BUS-1 downto 0);
 		node_step 					: in  STD_LOGIC;
 		data_in 						: in  STD_LOGIC_VECTOR (7 downto 0);
-		data_out 					: out STD_LOGIC_VECTOR (7 downto 0)
+		data_out0 					: out STD_LOGIC_VECTOR (7 downto 0);
+		data_out1 					: out STD_LOGIC_VECTOR (7 downto 0);
+		data_out2 					: out STD_LOGIC_VECTOR (7 downto 0);
+		state_out					: out STD_LOGIC
 	);
 end SIMD_ARRAY;
 
@@ -56,11 +59,31 @@ architecture Behavioral of SIMD_ARRAY is
 			sr_out					: out STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0)
 		);
 	end component;
+	
+	component NODE_EDGE is
+		Port (
+			clk 						: in  STD_LOGIC;
+			reset 					: in  STD_LOGIC;
+			instr 					: in  STD_LOGIC_VECTOR (NODE_IDATA_BUS-1 downto 0);
+			-- node_state 			: out STD_LOGIC;
+			n_in						: in STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+			s_in						: in STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+			e_in						: in STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+			w_in						: in STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+			n_out						: out STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+			s_out						: out STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+			e_out						: out STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+			w_out						: out STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);			
+			step 						: in  STD_LOGIC;
+			sr_in						: in  STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0);
+			sr_out					: out STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0)
+		);
+	end component;
 
 	constant ARRAY_COLS 			: integer := 3;
 	constant ARRAY_ROWS 			: integer := 3;
-	type DATA_T  is array (ARRAY_ROWS-1 downto 0) of STD_LOGIC_VECTOR ((ARRAY_COLS*8)-1 downto 0);
-	type STATE_T is array (ARRAY_ROWS-1 downto 0) of STD_LOGIC_VECTOR  (ARRAY_COLS-1 downto 0);
+	type DATA_T  is array (ARRAY_ROWS-1 downto 0) of STD_LOGIC_VECTOR (((ARRAY_COLS-1)*8)+7 downto 0);
+	type STATE_T is array (ARRAY_ROWS-1 downto 0) of STD_LOGIC_VECTOR   (ARRAY_COLS-1 downto 0);
 	
 	signal N_IN   					: DATA_T  := (others => (others=> '0'));
 	signal S_IN  					: DATA_T  := (others => (others=> '0'));
@@ -77,36 +100,57 @@ architecture Behavioral of SIMD_ARRAY is
 	signal STATE 					: STATE_T := (others => (others=> '0'));
 
 begin	
+	SR_IN(0)(((ARRAY_COLS-1)*8)-1 downto ((ARRAY_COLS-1)*8)-8) <= data_in;
+	SR_IN(1)(((ARRAY_COLS-1)*8)-1 downto ((ARRAY_COLS-1)*8)-8) <= data_in;
+	SR_IN(2)(((ARRAY_COLS-1)*8)-1 downto ((ARRAY_COLS-1)*8)-8) <= data_in;
+	
+	data_out0 <= SR_OUT(0)(((ARRAY_COLS-1)*8)+7 downto (ARRAY_COLS-1)*8);
+	data_out1 <= SR_OUT(1)(((ARRAY_COLS-1)*8)+7 downto (ARRAY_COLS-1)*8);
+	data_out2 <= SR_OUT(2)(((ARRAY_COLS-1)*8)+7 downto (ARRAY_COLS-1)*8);
+	
 	-- GENERATE ALL ROWS
-	GEN_ROWS : for row in 0 to ARRAY_ROWS-1 generate
+	GEN_ROW : for row in 0 to ARRAY_ROWS-1 generate
 		
 		-- GENERATE ALL COLS
-		GEN_COLS : for col in 0 to ARRAY_COLS-1 generate
+		GEN_COL : for col in 0 to ARRAY_COLS-1 generate
 			
 			-- GENERATE THE NODES
-			ARR_NODE : NODE port map (
+			GEN_NODE : NODE port map (
 				clk 					=> clk,
 				reset 				=> reset,
 				instr 				=> instr,
-				node_state 			=> STATE ( row   )((col*8)+7 downto col*8),
+				node_state 			=> STATE ( row )( col ),
 
-				n_in					=> N_IN  ( row   )((col*8)+7 downto col*8),
-				n_out					=> N_OUT ( row   )((col*8)+7 downto col*8),
+				n_in					=> N_IN  ( row )( (col*8)+7 downto col*8),
+				n_out					=> N_OUT ( row )( (col*8)+7 downto col*8),
 
-				w_in					=> W_IN  ( row   )((col*8)+7 downto col*8),
-				w_out					=> W_OUT ( row   )((col*8)+7 downto col*8),
+				w_in					=> W_IN  ( row )( (col*8)+7 downto col*8),
+				w_out					=> W_OUT ( row )( (col*8)+7 downto col*8),
 
-				s_in					=> S_IN  ( row+1 )((col*8)+7 downto col*8),
-				s_out					=> S_OUT ( row+1 )((col*8)+7 downto col*8),
+				s_in					=> S_IN  ( row )( (col*8)+7 downto col*8),
+				s_out					=> S_OUT ( row )( (col*8)+7 downto col*8),
 
-				e_in					=> E_IN  ( row+1 )((col*8)+7 downto col*8),
-				e_out					=> E_OUT ( row+1 )((col*8)+7 downto col*8),
+				e_in					=> E_IN  ( row )( (col*8)+7 downto col*8),
+				e_out					=> E_OUT ( row )( (col*8)+7 downto col*8),
 
 				step 					=> node_step,
-				sr_in					=> SR_IN ( row   )((col*8)+7 downto col*8),
-				sr_out				=> SR_OUT( row+1 )((col*8)+7 downto col*8)
+				sr_in					=> SR_IN ( row )( (col*8)+7 downto col*8),
+				sr_out				=> SR_OUT( row )( (col*8)+7 downto col*8)
 			);
-		end generate GEN_COLS;
-	end generate GEN_ROWS;
+		end generate GEN_COL;
+	end generate GEN_ROW;
+	
+	process (clk, reset) begin
+		if (reset = '1') then
+			state_out				<= '0';
+		elsif rising_edge(clk) then
+			if (STATE(0) = "000" and STATE(1) = "000" and STATE(2) = "000") then 
+				state_out			<= '0';
+			else
+				state_out			<= '1';
+			end if;
+		end if;
+	end process;
+	
 end Behavioral;
 
---
+++ fpga/node/NODE_EDGE.vhd
@@ -0,0 +1,144 @@
+----------------------------------------------------------------------------------
+-- Company				: TDT4295, NTNU
+-- Engineer				: HANS KRISTIAN FLAATTEN
+-- 
+-- Create Date			: 13:11:01 11/04/2012 
+-- Design Name			: FPGA NODE EDGE
+-- Module Name			: node - Behavioral 
+-- Project Name		: FPGA YTELSE
+-- Target Devices		: XILINX SPARTAN 3
+-- Tool versions		: ISE V11
+-- Description			:
+--
+-- Dependencies		: WORK.FPGA_CONSTANT_PKG
+--
+-- Revisions
+-- 0.01:  				File Created
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- "WORK" is the current library
+library WORK;
+use WORK.FPGA_CONSTANT_PKG.ALL;
+
+entity NODE_EDGE is
+	Port (
+		clk 							: in  STD_LOGIC;
+		reset 						: in  STD_LOGIC;
+		
+		instr 						: in  STD_LOGIC_VECTOR (NODE_IDATA_BUS-1 downto 0);
+		-- node_state 				: out STD_LOGIC;
+		
+		n_in							: in STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+		s_in							: in STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+		e_in							: in STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+		w_in							: in STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+
+		n_out							: out STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+		s_out							: out STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+		e_out							: out STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+		w_out							: out STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0);
+		
+		-- SDATA
+		step 							: in  STD_LOGIC;
+		sr_in							: in  STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0);
+		sr_out						: out STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0)
+	);
+end NODE_EDGE;
+
+architecture Behavioral of NODE_EDGE is
+----------------------------------------------------------------------------------
+--	NODE COMMINICATION
+----------------------------------------------------------------------------------
+	signal tmp_n_out				: STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0)	:= (others => '0');
+	signal tmp_s_out				: STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0)	:= (others => '0');
+	signal tmp_e_out				: STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0)	:= (others => '0');
+	signal tmp_w_out				: STD_LOGIC_VECTOR (NODE_DDATA_BUS-1 downto 0)	:= (others => '0');
+
+----------------------------------------------------------------------------------
+--	INSTRUCTION DECODER
+----------------------------------------------------------------------------------
+	signal ctrl_s_swap			: STD_LOGIC;
+
+----------------------------------------------------------------------------------
+--	SOURCE DATA REGISTER
+----------------------------------------------------------------------------------
+	component S_REG is
+		Port (
+			clk 						: in  STD_LOGIC;
+			reset						: in  STD_LOGIC;
+			s_swap 					: in  STD_LOGIC;
+			s_step 					: in  STD_LOGIC;
+			s_res 					: in  STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0);
+			s_in 						: in  STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0);
+			s_new 					: out STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0);
+			s_out 					: out STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0)
+		);
+	end component;
+		
+	signal sr_new					: STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0);
+	signal sr_data					: STD_LOGIC_VECTOR (NODE_SDATA_BUS-1 downto 0);
+
+begin
+
+----------------------------------------------------------------------------------
+--	SOURCE DATA REGISTER
+----------------------------------------------------------------------------------
+	NODE_S_REG : S_REG port map (
+		clk 							=> clk,
+		reset							=> reset,
+		s_swap						=> ctrl_s_swap,
+		s_res							=> sr_data,
+		s_new							=> sr_new,
+
+		s_step						=> step,
+		s_in							=> sr_in,
+		s_out							=> sr_out
+	);
+
+	process (clk, reset) begin
+		if (reset = '1') then
+			tmp_n_out				<= (others => '0');
+			tmp_s_out				<= (others => '0');
+			tmp_e_out				<= (others => '0');
+			tmp_w_out				<= (others => '0');
+		elsif rising_edge(clk) then
+			CASE instr(21 downto 19) IS
+				-- S-data swap
+				WHEN  NODE_INSTR_OP_S  =>
+					ctrl_s_swap 	<= '1';
+					sr_data			<= sr_new; -- check this
+				
+				-- Send source data
+				WHEN  NODE_INSTR_OP_M_SEND  =>
+					ctrl_s_swap 	<= '0';
+					
+					tmp_n_out		<= sr_data;
+					tmp_s_out		<= sr_data;
+					tmp_e_out		<= sr_data;
+					tmp_w_out		<= sr_data;
+				
+				-- Forward data
+				WHEN  NODE_INSTR_OP_M_FWRD  =>
+					ctrl_s_swap 	<= '0';
+					
+					tmp_n_out		<= e_in;
+					tmp_s_out		<= w_in;
+					tmp_e_out		<= n_in;
+					tmp_w_out		<= s_in;
+				
+				-- Do nothing
+				WHEN OTHERS =>
+					ctrl_s_swap		<= '0';
+			END CASE;
+		end if;
+		
+		n_out							<= tmp_n_out;
+		s_out							<= tmp_s_out;
+		e_out							<= tmp_e_out;
+		w_out							<= tmp_w_out;
+	end process;
+	
+end Behavioral;
+
