5e326a3904669644df2acb0eb40ce8082bda01a7
Vegard Edvardsen <evegard@hotmail.com>
2012-11-07 20:09:05 +0100

    Played around with the data loader code

--- fpga/data_loader.vhd
+++ fpga/data_loader.vhd
@@ -50,40 +50,18 @@ end data_loader;
 
 architecture Behavioral of data_loader is
 
---	signal incoming_big_word : std_logic_vector(23 downto 0);
 	signal big_word : std_logic_vector(23 downto 0);
-	
-	signal run : std_logic := '0';
-	signal done : std_logic := '1';
 	
 	signal cur_addr : std_logic_vector(RAM_DATA_ADDRESS_WIDTH - 1 downto 0) := (others => '0');
 	signal cur_word : natural range 0 to 2 := 0;
 
 begin
-
-	avr_interrupt <= done;
-	
---	update_incoming_big_word: process(clk)
---	begin
---		if rising_edge(clk) then
---			if avr_data_in_ready = '1' then
---				incoming_big_word <= avr_data_in;
---			end if;
---		end if;
---	end process;
 	
 	update_big_word: process(clk)
 	begin
 		if rising_edge(clk) then
 			if avr_data_in_ready = '1' then
 				big_word <= avr_data_in;
-				run <= '1';
-			else
-				if done = '0' then
-					run <= '1';
-				else
-					run <= '0';
-				end if;
 			end if;
 		end if;
 	end process;
@@ -91,27 +69,30 @@ begin
 	run_loader: process(clk)
 	begin
 		if rising_edge(clk) then
-			if run = '0' then
-				done <= '1';
-				mem_addr <= (others => '0');
-				mem_data <= (others => 'Z');
-				mem_write <= '1';
-			else
+			if cur_word /= 0 or avr_data_in_ready = '1' then
 				mem_addr <= cur_addr;
 				case cur_word is
-					when 0 => mem_data <= big_word(7 downto 0);
-					when 1 => mem_data <= big_word(15 downto 8);
-					when 2 => mem_data <= big_word(23 downto 16);
+					when 0 =>	mem_data <= big_word(7 downto 0);
+									cur_word <= 1;
+					when 1 =>	mem_data <= big_word(15 downto 8);
+									cur_word <= 2;
+					when 2 =>	mem_data <= big_word(23 downto 16);
+									cur_word <= 0;
 				end case;
 				mem_write <= '0';
 				
 				cur_addr <= conv_std_logic_vector(unsigned(cur_addr) + 1, RAM_DATA_ADDRESS_WIDTH);
-				cur_word <= cur_word + 1;
-				if cur_word = 0 then
-					done <= '1';
+				
+				if cur_word = 2 then
+					avr_interrupt <= '1';
 				else
-					done <= '0';
-				end if;
+					avr_interrupt <= '0';
+	 			end if;
+			else
+				mem_addr <= (others => '0');
+				mem_data <= (others => 'Z');
+				mem_write <= '1';
+				avr_interrupt <= '0';
 			end if;
 		end if;
 	end process;
