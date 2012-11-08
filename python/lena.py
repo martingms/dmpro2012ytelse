3c6ccc9c60be6b36ff53689828ffb02b8a7c7c97
Hans Kristian Flaatten <hans@starefossen.com>
2012-11-08 19:39:24 +0100

    cleaned up the fpga assembler

--- python/lena.py
+++ python/lena.py
@@ -1,79 +1,131 @@
 #!/usr/bin/env python
 import optparse
 
-# @todo: add support for mask bit
-# @todo: special things like move and mul?
+# @todo: add support for mask bit keyword
+# @todo: special instructions like move, mul and branch
 
-# --node add R3, R1, R2
-def __assembleNodeInstr__(instr, debug):
-	print instr
-	instr = instr.partition(" ");
-	fn = instr[0]
+verbose = False
+sep  = ""
+node = {
+	'regs' : {
+		'ZERO'		: "0000", 
+		'R1'		: "0001", 
+		'R2'		: "0010", 
+		'R3'		: "0011", 
+		'R4'		: "0100", 
+		'R5'		: "0101", 
+		'R6'		: "0110", 
+		'R7'		: "0111", 
+		'R8'		: "1000", 
+		'R9'		: "1001", 
+		'R10'		: "1010", 
+		'R11'		: "1011", 
+		'R12'		: "1100", 
+		'R13' 		: "1101", 
+		'R14' 		: "1110", 
+		'STATE' 	: "1111"
+	},
+	'instr' : {
+		# R-Format instruction
+		'r' : {
+			'add'	: '000', 
+			'sub'	: '001', 
+			'slt'	: '010', 
+			'and'	: '011', 
+			'or'	: '100', 
+			'eq'	: '101', 
+			'sll'	: '110',	# move?
+			'srl'	: '111'	# move?
+		},
+		# I-Format instructions
+		'i' : {
+			'addi' : '000', 
+			'subi' : '001', 
+			'slti' : '010', 
+			'andi' : '011', 
+			'ori' 	: '100', 
+			'eqi' 	: '101'
+		}
+	}
+}
+
+# Assemble Node R-Instructions
+def __assembleNodeInstrR__(fn, parmas):
+	bin  = "0" + sep + "0" + sep + "000" + sep 
+	regs = parmas.rsplit(" "); 
 	
-	if debug: 
-		sep = " "
-	else: 
-		sep = ""
+	if (len(regs) == 3):
+		for i in regs:
+			if i in node['regs']:
+				bin += node['regs'][i] + sep
+	else: exit("ERROR: R-Format Instructions expects exactly 3 input parameters.")
 
-	# Node registers address space
-	regs = { 'ZERO' : "0000", 'R1' : "0001", 'R2' : "0010", 'R3' : "0011", 'R4' : "0100", 'R5' : "0101", 'R6' : "0110", 'R7' : "0111", 'R8' : "1000", 'R9' : "1001", 'R10' : "1010", 'R11' : "1011", 'R12' : "1100", 'R13' : "1101", 'R14' : "1110", 'STATE' : "1111" }
+	bin += "0000" + sep + node['instr']['r'][fn]
+	return bin
 
-	# R-format instructions
-	rFormat = {'add' : '000', 'sub' : '001', 'slt' : '010', 'and' : '011', 'or' : '100', 'eq' : '101', 'sll' : '110', 'srl' : '111'}
-
-	# I-format instructions
-	iFormat = {'addi' : '000', 'subi' : '001', 'slti' : '010', 'andi' : '011', 'ori' : '100', 'eqi' : '101'}
-
-	# R-instruction assemble
-	if fn in rFormat: 
-		bin = "0" + sep + "0" + sep + "000" + sep 
-		reg = instr[2].rsplit(" "); 
-		
-		for i in reg:
-			if i in regs:
-				bin += regs[i] + sep
-		
-		bin += "0000" + sep + rFormat[fn]
-		print bin 	
+# Assemble Node I-Instruction
+def __assembleNodeInstrI__(fn, parmas):
+	bin  = "0" + sep + "0" + sep + "001" + sep 
+	regs = parmas.rsplit(" "); 
 	
-	# I-instruction assemble
-	elif fn in iFormat:
-		bin = "0" + sep + "0" + sep + "001" + sep 
-		reg = instr[2].rsplit(" "); 
-		
-		for i in reg:
-			if i in regs:
-				bin += regs[i] + sep
+	# @todo check for constant
+	if (len(regs) == 3):
+		for i in regs:
+			if i in node['regs']:
+				bin += node['regs'][i] + sep
 			elif i.isdigit():
 				bin += dec2bin(i, 8) + sep
+	else: exit("ERROR: I-Format Instructions expects exactly 3 input parameters.")
+
+	bin += node['instr']['i'][fn]
+	return bin
+
+# Assemble a NODE instruction into binary code 
+def __assembleNodeInstr__(instr):
+	debug("Assembling SIMD Node instruction '" + instr + "'")
 	
-		bin += iFormat[fn]
-		print bin 	
+	instr 		= instr.partition(" ");
+	fn 			= instr[0]
+	params 	= instr[2]
 	
-	# Format not found
-	else:
-		print "command not found"
+	if fn in node['instr']['r']	: output = __assembleNodeInstrR__(fn, params) # R-Functions
+	elif fn in node['instr']['i']	: output = __assembleNodeInstrI__(fn, params) # I-Functions
+	else								: exit("ERROR: Unrecognized instruction '" + fn + " " + params + "'")
+	
+	#debug(output)
+	return output
+
+# Print (if verbose)
+def debug(str) :
+	if verbose:
+		print(str)
 
 # Decimal to binary converter
 def dec2bin( dec, target ):
 	b = bin(int(dec));
 	b = b[2:(target+2)]
-	
-	for i in range(len(b), target):
-		b = '0' + b
-		
+
+	# Add target length padding
+	for i in range(len(b), target) : b = '0' + b
+
 	return b
 
 # Main function
 def main():
+	global sep, verbose
+	
 	p = optparse.OptionParser()
-	p.add_option('--node', '-n', default="add R3 R1 R2")
-	p.add_option('--debug', '-d', action="store_true", default=False)
+	p.add_option('--node', '-n', default="add R3 R1 R2", help="assemble a single SIMD Node instruction", metavar="INSTR")
+	p.add_option('--debug', '-d', action="store_true", default=False, help="")
+	p.add_option('--verbose', '-v', action="store_true", default=False, help="print status messages to stdout")
 	options, arguments = p.parse_args()
 	
-	if options.node:
-			__assembleNodeInstr__(options.node, options.debug)
-	else: print "unknown command"
+	# Debug seperator
+	if options.debug: sep = " "
+	if options.verbose: verbose = " "
+	
+	if options.node				: print __assembleNodeInstr__(options.node)
+	else							: exit("Unknown command line tool command")
 
 if __name__ == '__main__':
 	main()
