9527bc2cd4da24dbe11f15981fb98380b0f32c74
Håkon Wikene <haakonwikene@gmail.com>
2012-11-14 21:20:54 +0100

    interrupt-less timer in avr

--- avr/src/timer/timer.c
+++ avr/src/timer/timer.c
@@ -18,22 +18,31 @@ static void rtc_irq(void) {
 // should be called after Disable_global_interrupt
 // and INTC_init_interrupt, before Enable_global_interrupt
 void timer_init() {
-	INTC_register_interrupt(&rtc_irq, AVR32_RTC_IRQ, AVR32_INTC_INT0);
+//	INTC_register_interrupt(&rtc_irq, AVR32_RTC_IRQ, AVR32_INTC_INT0);
 	if (!rtc_init(&AVR32_RTC, RTC_OSC_RC, TIMER_PSEL)) {
 		LED_On(LED7);LED_On(LED6);LED_On(LED5);LED_On(LED4);LED_On(LED3);
 		while(1);
 	}
 
-	rtc_set_top_value(&AVR32_RTC, 0);
-	rtc_enable_interrupt(&AVR32_RTC);
+//	rtc_set_top_value(&AVR32_RTC, 0);
+//	rtc_enable_interrupt(&AVR32_RTC);
 	rtc_enable(&AVR32_RTC);
 }
 
-// gets time time in milliseconds since initialization
+// gets time in milliseconds since initialization
 double timer_get_ms() {
-	return 1000.0 * irq_counter / TICKS_PER_SEC;
+	return 1000.0 * rtc_get_value(&AVR32_RTC) / TICKS_PER_SEC;
+//	return 1000.0 * irq_counter / TICKS_PER_SEC;
 }
 
 void timer_reset() {
+	rtc_set_value(&AVR32_RTC, 0);
 	irq_counter = 0;
+}
+
+void timer_sleep(int ms) {
+	int tick_diff = ms * TICKS_PER_SEC / 1000;
+	int start = rtc_get_value(&AVR32_RTC);
+	while (rtc_get_value(&AVR32_RTC) - start < tick_diff);
+//	while (irq_counter - start < tick_diff);
 }
