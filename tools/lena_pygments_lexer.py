091e6504de8115d5577cec864bd6ea8b8d311684
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-11-17 16:16:34 +0100

    Do not use 100% CPU when running lena-pygmentize.

--- tools/lena_pygments_lexer.py
+++ tools/lena_pygments_lexer.py
@@ -17,27 +17,32 @@ class LenaLexer(RegexLexer):
 
     tokens = {
         'root': [
+            include('whitespace'),
             include('keyword'),
-            (r'#.*?$', Comment),
             (r'[0-9]+', Number.Integer)
+        ],
+        'whitespace': [
+            (r'\n', Text),
+            (r'\s+', Text),
+            (r'#.*?\n', Comment)
         ],
         'keyword': [
             # Namespace
-            (r'|ctrl|node', Keyword.Namespace),
+            (r'(ctrl|node)', Keyword.Namespace),
 
             # Keywords
-            (r'|nop|add|sub|eq|slt|and|or|addi|subi|eqi|slti'
+            (r'(nop|addi|sub|eq|slt|and|or|add|subi|eqi|slti'
              r'|andi|ordi|sll|srl|beq|branch|jump|jump|move|swap'
-             r'|send|store|fwrd|lw|dma', Keyword),
+             r'|send|store|fwrd|lw|dma)', Keyword),
 
             # DMA
-            (r'|set_read_active|set_read_base_addr'
+            (r'(set_read_active|set_read_base_addr'
              r'|set_read_horizontal_incr set_read_vertical_incr'
              r'|set_write_active|set_write_base_addr|set_write_horizontal_incr'
-             r'|set_write_vertical_incr start', Keyword.Type), #Using random token just to get some colors yo.
+             r'|set_write_vertical_incr|start)', Keyword.Type), #Using random token just to get some colors yo.
 
             # Registers
-            (r'|R0|R1|R2|R3|R4|R5|R6|R7|R8|R9|R10|R11|R12|R13|R14|R15'
-             r'|VDATA|VADDR|ZERO|DMA', Name.Builtin)
+            (r'(R0|R1|R2|R3|R4|R5|R6|R7|R8|R9|R10|R11|R12|R13|R14|R15'
+             r'|VDATA|VADDR|ZERO|DMA)', Name.Builtin)
         ]
     }
